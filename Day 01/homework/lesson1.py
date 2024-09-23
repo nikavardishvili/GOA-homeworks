e7bc4290001cc1a9da728669ca6ddaa63782c9b0
nikavardishvili <160754682+nikavardishvili@users.noreply.github.com>
2024-09-23 18:59:31 +0400

    commit

---
+++ Day 01/homework/lesson1.py
@@ -0,0 +1,11 @@
+Name = "nika"
+
+Surname = "vardishvili"
+
+Age = 15
+
+Height = 1.72
+
+Class = "10"
+
+print("me var " + Name + " " + Surname + " " + str(Age) + " " + "wlis" + " " + "var" + " " + str(Height) + "sm" + " " + "var" + " " + Class + " " + "klasshi")
---
+++ Day 02/classwork/classowork.py
@@ -0,0 +1,44 @@
+# 1)
+
+num1 = 5
+num2 = 7
+
+sum = num1 + num2
+
+print(sum)
+
+# 2)
+
+price = 50
+
+
+
+# # 3)
+
+# # debugging aris shecdomebis shesworeba
+
+print("Hello, World!")
+      
+greeting = "Good Morning"
+print(greeting)
+
+first_name = "Alice"
+last_name = "Smith"
+print("Name: "+ first_name + " " + last_name)
+
+number = 50
+
+print("The number is: " + str(number))
+
+name = "George"
+
+print("hello" + " " + name)
+
+
+age = 25
+print("I am" + " " + str(age) + " " + "years old.")
+
+num1 = 10
+num2 = 20
+result = num1 + num2
+print(result)
